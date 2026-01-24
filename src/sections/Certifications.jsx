29aa358f81c080341f89e1edb2bbd924461adc32
Thiago Gabriel <thiagogab.20@gmail.com>
2026-01-24 01:16:32 -0300

    feat: certificados implantados

--- src/sections/Certifications.jsx
+++ src/sections/Certifications.jsx
@@ -12,16 +12,22 @@ export default function Certifications() {
       "/certificates/cert-1.png",
       "/certificates/cert-2.png",
       "/certificates/cert-3.png",
+      "/certificates/cert-4.png",
+      "/certificates/cert-5.png",
     ],
     startups: [
+      "/certificates/startup5.png",
       "/certificates/startup1.png",
       "/certificates/startup2.png",
       "/certificates/startup3.png",
+      "/certificates/startup4.png",
     ],
     outros: [
-      "/certificates/react1.png",
-      "/certificates/frontend1.png",
-      "/certificates/git1.png",
+      "/certificates/outros1.png",
+      "/certificates/outros2.png",
+      "/certificates/outros3.png",
+      "/certificates/outros4.png",
+      "/certificates/outros5.png",
     ],
   };
 
@@ -61,10 +67,11 @@ export default function Certifications() {
             <div className="divider" />
 
             <ul>
-              <li>Iniciação Científica (CIPEEX)</li>
-              <li>Engenharia de Software</li>
-              <li>Arquitetura de Computadores</li>
-              <li>Programação e Estruturas de Dados</li>
+              <li>Conclusão do Artigo Científico</li>
+              <li>Congresso Internacional de Pesquisa, Ensino e Extensão (CIPEEX)</li>
+              <li>Código Inteligente: A Lógica Criacional e a Inteligência por Trás dos Sistemas</li>
+              <li>Workshop de Design Thinking</li>
+              <li>Iniciação Científica</li>
             </ul>
 
             <button
@@ -81,9 +88,12 @@ export default function Certifications() {
             <div className="divider" />
 
             <ul>
-              <li>Empreendedorismo</li>
-              <li>Gestão de Negócios</li>
-              <li>Inovação e Startups</li>
+              <li>2º lugar no Agrostartup 2025</li>
+              <li>Capacitação Empreendedora</li>
+              <li>Conclusão da Capacitação Empreendedora</li>
+              <li>Olimpíada de Empreendedorismo - UFG</li>
+              <li>Vitória no 2º Hackathon</li>
+
             </ul>
 
             <button
@@ -100,9 +110,11 @@ export default function Certifications() {
             <div className="divider" />
 
             <ul>
-              <li>Front-end (HTML, CSS, JavaScript)</li>
-              <li>React</li>
-              <li>Versionamento com Git</li>
+              <li>Jornada Full Stack</li>
+              <li>Maratona de Inovação - Anápolis</li>
+              <li>VI Curso de Pesquisa e Inovação - Workshop</li>
+              <li>Oficina - In_Book: Comunicação e Liderança</li>
+              <li>Maratona Empreendedora </li>
             </ul>
 
             <button
